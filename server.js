40f54c68349c16685ac2990098dc718ff2373d30
BraydonKains <kainsbraydon@gmail.com>
2020-05-29 11:46:20 -0400

    Logging ip of requester

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 const PORT = 8000;
 
 const GET = 'GET';
@@ -20,6 +21,18 @@ http.createServer((req, res) => {
 			res.write('Boom, you lookin for this?');
 			res.end();
 		})
+	}
+	if(req.method === 'POST' && req.url === '/') {
+		let ip = req.headers['x-forwarded-for'] || 
+			     req.connection.remoteAddress || 
+			     req.socket.remoteAddress ||
+			     (req.connection.socket ? req.connection.socket.remoteAddress : null);
+		let message = `Request made from ${ip}\n`;
+		console.log(message);
+		fs.appendFile('log.txt', message, (err) => console.log(err));	
+		res.writeHead(200, {'Content-Type': 'text/plain'});
+		res.write('Boom, you lookin for this?');
+		res.end();
 	}
 }).listen(PORT, () => {
 	console.log(`listening on ${PORT}`);
